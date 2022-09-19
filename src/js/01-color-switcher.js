1c02ccbedaa3bb7deb8b7b8b26fab181f6cfd799
leshazimoglyad <102586567+leshazimoglyad@users.noreply.github.com>
2022-09-19 11:46:43 +0300

    Dz9 update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -65,20 +65,19 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-// references for buttons
+
 const refs = {
   startBtn: document.querySelector('[data-start]'),
   stopBtn: document.querySelector('[data-stop]'),
 };
 const { startBtn, stopBtn } = refs;
-// default value of timer ID
+
 let timerId = null;
 
-// event listeners
+
 startBtn.addEventListener('click', onChangeColorBtn);
 stopBtn.addEventListener('click', onStopColorBtn);
 
-// function for StartBtn for changing background color where we return another function to call first changing bg color withoud delay
 function onChangeColorBtn() {
   timerId = setInterval(
     (function toImmediateChangeBgColor() {
@@ -89,11 +88,10 @@ function onChangeColorBtn() {
     1000
   );
 
-  // set "disabled" attribute just not to let create "hell of intervals"
+  
   startBtn.setAttribute('disabled', '');
 }
 
-// function for stopping changing bg color(remove disabled attribute for startBtn)
 function onStopColorBtn() {
   clearInterval(timerId);
   startBtn.removeAttribute('disabled', '');
--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -18,7 +18,7 @@ const {
   minutesSpan,
   secondsSpan,
 } = refs;
-// default variables
+
 let currentTime = new Date();
 let selectedDatesUTC = 0;
 let intervalId = null;
@@ -62,18 +62,18 @@ function toStartTimer() {
 }
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
+  
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
-  // Remaining days
+  
   const days = Math.floor(ms / day);
-  // Remaining hours
+  
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minute`s
+  
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
+  
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
   return { days, hours, minutes, seconds };
 }
